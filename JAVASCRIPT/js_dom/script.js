c75a1d340796b874c9d69df7755bf1f80bb4fa13
Rajit kumar <krajit909@gmail.com>
2023-12-25 14:42:10 +0530

    task completed

--- JAVASCRIPT/js_dom/script.js
+++ JAVASCRIPT/js_dom/script.js
@@ -16,46 +16,81 @@ where Delta is bold.
 Solution--
 */
 
-let Body = document.querySelector("body")
-let LineBreak = document.createElement("br")
-let h1 = document.createElement("h1")
+// let Body = document.querySelector("body")
+// let LineBreak = document.createElement("br")
+// let h1 = document.createElement("h1")
 
 
-let input = document.createElement("input")
-input.setAttribute("Placeholder", "Username")
+// let input = document.createElement("input")
+// input.setAttribute("Placeholder", "Username")
 
-let btn = document.createElement("button")
-btn.setAttribute("type", "submit")
-btn.setAttribute("id", "btn")
-
-
-btn.innerText = "Click me"
-btn.style.padding = "5px 15px"
-btn.style.marginTop = "12px"
-btn.style.backgroundColor = "blue"
-btn.style.color = "white"
-btn.style.fontWeight = "700"
+// let btn = document.createElement("button")
+// btn.setAttribute("type", "submit")
+// btn.setAttribute("id", "btn")
 
 
-let underline = document.createElement("u")
-h1.innerText = "Dom Practice"
+// btn.innerText = "Click me"
+// btn.style.padding = "5px 15px"
+// btn.style.marginTop = "12px"
+// btn.style.backgroundColor = "blue"
+// btn.style.color = "white"
+// btn.style.fontWeight = "700"
+
+
+// let underline = document.createElement("u")
+// h1.innerText = "Dom Practice"
 
 
 
-Body.appendChild(underline)
-underline.appendChild(h1)
+// Body.appendChild(underline)
+// underline.appendChild(h1)
 
-let para = document.createElement("p")
-para.innerHTML = "Apna College <b>Delta</b> Practice"
+// let para = document.createElement("p")
+// para.innerHTML = "Apna College <b>Delta</b> Practice"
 
-Body.appendChild(input)
-Body.appendChild(LineBreak)
-Body.appendChild(btn)
-Body.appendChild(para)
+// Body.appendChild(input)
+// Body.appendChild(LineBreak)
+// Body.appendChild(btn)
+// Body.appendChild(para)
 
-let text = document.querySelector("#inputText")
-function handleSubmit(){
-    // alert("Text Submitted!.");
-    console.log(text.value);
-    text.value = text.ariaPlaceholder
+// let text = document.querySelector("#inputText")
+// function handleSubmit(){
+//     // alert("Text Submitted!.");
+//     console.log(text.value);
+//     text.value = text.ariaPlaceholder
+// }
+
+/* Random color genrator */
+let outer = document.querySelector("body")
+let h1 = document.querySelector("h1")
+let btn = document.querySelector(".btn")
+let bgColor = document.querySelector(".inr-container")
+
+btn.addEventListener("click", function(){
+    let randomColor = getRandomColor()
+    h1.innerText = randomColor;
+    bgColor.style.background = randomColor;
+
+
+ 
+    outer.style.background = outerColor()
+    btn.style.background = outerColor()
+    
+})
+
+function getRandomColor(){
+    let red = Math.floor(Math.random() * 255 + 1);
+    let green = Math.floor(Math.random() * 255 +1);
+    let blue = Math.floor(Math.random() * 255 +1);
+
+    let color = `rgb(${red}, ${green}, ${blue})`
+    return color;
 }
+
+function outerColor(){
+    let red = Math.floor(Math.random() * 255 + 1);
+    let green = Math.floor(Math.random() * 255 +1);
+    let blue = Math.floor(Math.random() * 255 +1);
+
+    let color = `rgb(${red}, ${green}, ${blue})`
+    return color;
