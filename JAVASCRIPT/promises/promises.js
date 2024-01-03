cf86ecd441fd950ed33068947b0c6aac5ad6aa42
Rajit kumar <krajit909@gmail.com>
2024-01-03 22:22:31 +0530

    bug fix

---
+++ JAVASCRIPT/promises/promises.js
@@ -0,0 +1,18 @@
+function savetoDb(data){
+    return new Promise((resolve, reject) => {
+        let internetSpeed = Math.floor(Math.random() * 10) +1;
+        if (internetSpeed >4) {
+            resolve("Success: data was saved")
+        }else{
+            reject("Failure: weak connection");
+        }
+    });
+};
+
+savetoDb("hii")
+.then(()=>{
+    console.log(resolve)
+})
+.catch(()=>{
+console.log(reject);
+})
